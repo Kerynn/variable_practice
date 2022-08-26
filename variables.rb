ef17f84451b939b1baf91a5a23bf9a5aacb76015
Kerynn Davis <kerynn.davis@gmail.com>
2022-08-25 20:03:59 -0700

    Reassigned variables and puts statements

--- variables.rb
+++ variables.rb
@@ -11,3 +11,18 @@ paid = 35.00
 portland_resident = true
 best_dog = true
 owns_house = false
+dog_names = "Kamee"
+email = "helloyou@gmail.com"
+home_state = "Oregon"
+dog_age = 9
+pets_owned = 3
+work_assignments = 5
+dog_fee = 13.00
+amount_owed = 24.00
+paid = 54.00
+portland_resident = false
+best_dog = false
+owns_house = true
+puts "Give #{dog_names} a charge of #{dog_fee} to stay at the house."
+puts "Send #{email} a notice if #{owns_house}."
+puts "Work on #{work_assignments} tasks if lives in #{home_state}."
